bbd2cb8a90b658af5fec20270e03f3db00bca33e
semvs <semvs@ayvansaray.edu.tr>
2019-01-02 13:30:07 +0300

    Add project files.

---
+++ DelegateApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateApp
+{
+    class Program
+    {
+        delegate int MyDelegate(int a);
+        static void Main(string[] args)
+        {
+            MyDelegate del = new MyDelegate(kareAl);
+            Console.WriteLine($"5'in Karesi : {del(5)}");
+            MyDelegate my = delegate (int a) { return (a * a) };
+            Console.ReadLine();
+            
+        }
+        public static int kareAl(int a)
+        {
+            return (a*a);
+        }
+    }
+}
