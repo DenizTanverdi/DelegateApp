00873525d84b7caf4f92e77e8f8affe324903d5b
semvs <semvs@ayvansaray.edu.tr>
2019-01-02 14:10:30 +0300

    Link

--- DelegateApp/Program.cs
+++ DelegateApp/Program.cs
@@ -40,8 +40,14 @@ namespace DelegateApp
         {
             int[] tumRakamlar = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             IEnumerable<int> tekRakamlar = tumRakamlar.Where(x => x % 2 == 1);
+            IEnumerable<int> ciftRakamlar = tumRakamlar.Where(x => x % 2 == 0);
             Console.WriteLine("Tek Rakamlar : ");
             foreach (int rakam in tekRakamlar)
+            {
+                Console.WriteLine(rakam);
+            }
+            Console.WriteLine("Cift Rakamlar : ");
+            foreach (int rakam in ciftRakamlar)
             {
                 Console.WriteLine(rakam);
             }
